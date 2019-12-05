cea4e7713dcd6de8e7d9d8990e5fd23f04a155ad
rargirov <rargirov@gmail.com>
2019-12-05 06:57:29 +0200

    Small changes to Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -17,17 +17,9 @@ const Header = () => {
   return (
     <header style={styles}>
       <h1>To-Do</h1>
-      <Link style={linkStyle} to="/">
-        Home
-      </Link>{" "}
-      {" | "}
-      <Link style={linkStyle} to="/about">
-        About
-      </Link>{" "}
-      {" | "}
-      <Link style={linkStyle} to="/contacts">
-        Contacts
-      </Link>
+      <Link style={linkStyle} to="/">Home | </Link>
+      <Link style={linkStyle} to="/about"> About | </Link>
+      <Link style={linkStyle} to="/contacts"> Contacts</Link>
     </header>
   );
 };
